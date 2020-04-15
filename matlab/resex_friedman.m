311da6fd1b1d0db0d91228e757eaf4fe614946b8
David Tax <dtax@tud0035368.home>
2020-04-15 10:24:55 +0200

    example friedman test

---
+++ matlab/resex_friedman.m
@@ -0,0 +1,18 @@
+% Generate (not completely random) results to show:
+m = repmat((1:10)'/10,[1,4,5]);
+res = m + 0.4*rand(10,4,5);
+
+% Define the values of the different dimensions:
+dim1 = {'cl1','cl2','cl3','cl4','cl5','cl6',...
+   'cl7','cl8','cl9','cl10'};
+dim2 = {'data1','iris','ionosphere','dat4'};
+dim3 = (1:5)';
+
+% Store it in a results object:
+R = results(res,dim1,dim2,dim3);
+% Set the dimension names:
+R = setdimname(R,'classifiers','datasets','runs');
+% Set the results name:
+R = setname(R,'AUC performances');
+
+testfriedman(R,1)
